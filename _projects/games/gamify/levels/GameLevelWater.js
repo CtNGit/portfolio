a0575f2d2e9aeeb0b1a474519af2764aeacd37cd
CtNGit <casey.tri.nguyen@gmail.com>
2026-09-11 17:52:11 -0700

    pls

--- _projects/games/gamify/levels/GameLevelWater.js
+++ _projects/games/gamify/levels/GameLevelWater.js
@@ -17,7 +17,7 @@ class GameLevelWater {
     let path = gameEnv.path;
 
     // Background data
-    const image_src_water = path + "/images/projects/gamify/water/deepseadungeon.jpeg";
+    const image_src_water = path + "/images/projects/gamify/water/coralreef.jpg";
     const image_data_water = {
         id: 'Water',
         src: image_src_water,
@@ -88,7 +88,7 @@ class GameLevelWater {
       };
 
      // Shark Data
-    const sprite_src_shark = path + "/images/projects/gamify/water/shark.png"; // be sure to include the path
+    const sprite_src_shark = path + "/images/projects/gamify/water/piranhas.png"; // be sure to include the path
     const sprite_data_shark = {
         id: 'Shark',
         greeting: "Enemy Shark",
