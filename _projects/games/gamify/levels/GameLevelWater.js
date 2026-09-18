9be4a3ede6edbfd99e04c1a21d94c9c25f27e574
CtNGit <casey.tri.nguyen@gmail.com>
2026-09-17 21:52:51 -0700

    sv

--- _projects/games/gamify/levels/GameLevelWater.js
+++ _projects/games/gamify/levels/GameLevelWater.js
@@ -17,7 +17,7 @@ class GameLevelWater {
     let path = gameEnv.path;
 
     // Background data
-    const image_src_water = path + "/images/projects/gamify/water/coralreef.jpg";
+    const image_src_water = path + "/images/projects/gamify/water/coralreef (1).jpg";
     const image_data_water = {
         id: 'Water',
         src: image_src_water,
